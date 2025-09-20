4f7008aa1bdc5746b4480c785e77e608c24c8648
ievlev_alex <ievlev@outlook.com>
2025-09-20 22:13:14 +0300

    Sprint 15 v.1.1

--- app.js
+++ app.js
@@ -21,10 +21,21 @@ mongoose
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://what2wear.undo.it",
+      ];
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(new Error("Not allowed by CORS"), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 
 app.use(requestLogger);
